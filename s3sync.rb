26e41dd6ce9e45a21e045c5db1cde5068b4e9171
Ryan Baumann <ryan.baumann@gmail.com>
2011-12-13 17:03:37 -0500

    Only transfer needed files

--- s3sync.rb
+++ s3sync.rb
@@ -3,6 +3,17 @@
 require 'rubygems'
 require 'aws/s3'
 
+def do_transfer?(remote,filename,bucket)
+  begin
+    if ((!AWS::S3::S3Object.exists?(remote, bucket)) || (File.mtime(filename) > Time.parse(AWS::S3::S3Object.find(remote,bucket).about['last-modified'])))
+      return true
+    end
+  rescue
+    return true
+  end
+  return false
+end
+
 AWS::S3::Base.establish_connection!(
   :access_key_id     => ENV['AMAZON_ACCESS_KEY_ID'],
   :secret_access_key => ENV['AMAZON_SECRET_ACCESS_KEY']
@@ -14,25 +25,27 @@ AWS::S3::Bucket.enable_logging_for(bucket)
 Dir.glob("_site/**/*") do |filename|
   unless File.directory? filename
     remote = filename.sub(/^_site\//,'')
-    puts filename + " -> " + remote
-    
-    options_hash = {:access => :public_read}.merge(
-      (remote =~ /^archives\//) ? {:content_type => 'text/html'} : {}
-    )
-    AWS::S3::S3Object.store(
-      remote,
-      open(filename),
-      bucket,
-      options_hash
-    )
-
-    if remote =~ /^archives\//
+    if do_transfer?(remote,filename,bucket)
+      puts filename + " -> " + remote
+      
+      options_hash = {:access => :public_read}.merge(
+        (remote =~ /^archives\//) ? {:content_type => 'text/html'} : {}
+      )
       AWS::S3::S3Object.store(
-        remote + '/',
+        remote,
         open(filename),
         bucket,
         options_hash
       )
+
+      if remote =~ /^archives\//
+        AWS::S3::S3Object.store(
+          remote + '/',
+          open(filename),
+          bucket,
+          options_hash
+        )
+      end
     end
   end
 end
